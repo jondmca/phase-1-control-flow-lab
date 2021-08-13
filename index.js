24d71c68cee0db6b42deece5dd658e1800b700cd
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-08-12 19:11:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let response;
+  if (feet <= 400){
+    response = 'This one is on me!';
+  }
+  else if (feet > 2000 && feet <= 2500){
+    response = 'I will gladly take your thirty bucks.';
+  }
+  else if(feet > 2500 ){
+    response = 'No can do.';
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let saying = (city === 'NYC') ? "Ok, sounds good." : "No go.";
+  return saying;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let charmResponse;
+  switch (tip){
+    case 'generous':
+      charmResponse = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charmResponse = 'Thank you.';
+      break;
+    default:
+      charmResponse = 'Bye.';
+      break;
+  }
+  return charmResponse;
+  
 }
